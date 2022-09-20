284fb164d67f43ca1bf794b95b69a8745fd1b822
陈君豪 <2711046020@qq.com>
2022-09-20 15:40:09 +0800

    设置启动分辨率

---
+++ Content/Script/Blueprints/MainGameMode.lua
@@ -0,0 +1,36 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+---@class MainGameMode
+local MainGameMode = Class()
+
+--function MainGameMode:Initialize(Initializer)
+--end
+
+--function MainGameMode:UserConstructionScript()
+--end
+
+--function MainGameMode:ReceiveBeginPlay()
+--end
+
+--function MainGameMode:ReceiveEndPlay()
+--end
+
+-- function MainGameMode:ReceiveTick(DeltaSeconds)
+-- end
+
+--function MainGameMode:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+--end
+
+--function MainGameMode:ReceiveActorBeginOverlap(OtherActor)
+--end
+
+--function MainGameMode:ReceiveActorEndOverlap(OtherActor)
+--end
+
+return MainGameMode
